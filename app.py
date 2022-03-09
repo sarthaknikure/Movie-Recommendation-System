03ae466234debe8c1fa17db44e66d33696dcb4ec
sarthaknikure <47851081+sarthaknikure@users.noreply.github.com>
2022-03-09 08:08:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
-import pickle
+import pickle5 as pickle
 import requests
 
 
